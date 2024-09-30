cd3a35703b192d99dc23efc196107bae8241b67d
arpan jana <arpangroup1@gmail.com>
2024-10-01 02:56:27 +0530

    log module

---
+++ sre-logging/src/main/java/com/observability/sre_logging/SreLoggingApplication.java
@@ -0,0 +1,13 @@
+package com.observability.sre_logging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SreLoggingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SreLoggingApplication.class, args);
+	}
+
+}
